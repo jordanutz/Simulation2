5ff4a90a1b8a7ff5ca1d7f0777588514232b3737
jordanutz <jordanutz@outlook.com>
2019-02-01 08:54:15 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Reset.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Simulation 2
+      </div>
+    );
+  }
+}
+
+export default App;
